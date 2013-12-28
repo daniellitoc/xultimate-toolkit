69b1c1b21544833ba4ca44433579a8f843ef8ab7
Daniel Li <daniellitoc@gmail.com>
2013-12-28 15:01:52 +0800

    增加ThreadUtils

---
+++ xultimate-core/src/main/java/org/danielli/xultimate/util/thread/ThreadUtils.java
@@ -0,0 +1,15 @@
+package org.danielli.xultimate.util.thread;
+
+public class ThreadUtils {
+
+	/**
+	 * 当前线程等待，直到活跃线程数小于等于activeThreadCount。
+	 * @param activeThreadCount 目标活跃线程数目。
+	 */
+	public static void waitUntilLe(int activeThreadCount) {
+		while (Thread.activeCount() > activeThreadCount) {
+			Thread.yield();
+		}
+	}
+	
+}
