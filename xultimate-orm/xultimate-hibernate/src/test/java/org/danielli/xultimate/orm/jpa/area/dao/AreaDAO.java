4d96bfadfa871ba6f21d2c13aa6c2eb0a90b602c
Daniel Li <daniellitoc@gmail.com>
2014-03-01 16:07:22 +0800

    添加Spring Data JPA提供的基于@Query形式的扩展。

--- xultimate-orm/xultimate-hibernate/src/test/java/org/danielli/xultimate/orm/jpa/area/dao/AreaDAO.java
+++ xultimate-orm/xultimate-hibernate/src/test/java/org/danielli/xultimate/orm/jpa/area/dao/AreaDAO.java
@@ -4,9 +4,16 @@ import java.util.List;
 
 import org.danielli.xultimate.orm.jpa.GenericDAO;
 import org.danielli.xultimate.orm.jpa.area.domain.Area;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AreaDAO extends GenericDAO<Area, Long>, AreaDAOExt {
     
     List<Area> findByName(String name);
+    
+    @Query("FROM Area WHERE name = :name")
+    Page<Area> findByQuery(@Param("name") String name, Pageable pageable);
     
 }
