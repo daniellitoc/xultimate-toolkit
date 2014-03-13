a4ba3a0faadfbf27a3b6010bf4572c1393f049ab
Daniel Li <daniellitoc@gmail.com>
2014-03-13 22:47:19 +0800

    修改JSON部分。

--- xultimate-core/src/main/java/org/danielli/xultimate/core/json/JsonUtils.java
+++ xultimate-core/src/main/java/org/danielli/xultimate/core/json/JsonUtils.java
@@ -1,5 +1,15 @@
 package org.danielli.xultimate.core.json;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Writer;
+
+import org.danielli.xultimate.core.json.fastjson.ValueTypeAdapter;
+import org.danielli.xultimate.util.CharsetUtils;
+import org.danielli.xultimate.util.io.IOUtils;
+
+import com.alibaba.fastjson.JSON;
+
 /**
  * JSON工具类。
  * 
@@ -22,5 +32,83 @@ public class JsonUtils {
 		jsonpBuilder.append(jsonBody);
 		jsonpBuilder.append(")");
 		return jsonpBuilder.toString();
+	}
+	
+	/**
+     * Method that can be used to serialize any Java value as
+     * a String. Functionally equivalent to calling
+     * {@link #writeValue(Writer,Object)} with {@link java.io.StringWriter}
+     * and constructing String, but more efficient.
+     */
+	public static <T> String writeValueAsString(T value) {
+		try {
+			return JSON.toJSONString(value);
+		} catch (Exception e) {
+			throw new JSONException(e.getMessage(), e);
+		} 
+	}
+	
+	/**
+     * Method that can be used to serialize any Java value as
+     * a byte array. Functionally equivalent to calling
+     * {@link #writeValue(Writer,Object)} with {@link java.io.ByteArrayOutputStream}
+     * and getting bytes, but more efficient.
+     * Encoding used will be UTF-8.
+     */
+	public static <T> byte[] writeValueAsBytes(T value) throws JSONException {
+		try {
+			return JSON.toJSONBytes(value);
+		} catch (Exception e) {
+			throw new JSONException(e.getMessage(), e);
+		} 
+	}
+	
+	/**
+     * Method that can be used to serialize any Java value as
+     * JSON output, using output stream provided (using encoding
+     * {@link CharsetUtils#UTF_8}).
+     */
+	public static <T> void writeValue(OutputStream out, T value) throws JSONException {
+		try {
+			IOUtils.write(JSON.toJSONBytes(value), out);
+		} catch (Exception e) {
+			throw new JSONException(e.getMessage(), e);
+		} 
+	}
+	
+    /**
+     * Method that can be used to serialize any JSON output as
+     * Java value.
+     */
+	public static <T> T readValue(String content, ValueType<T> valueType) throws JSONException {
+		try {
+			return JSON.parseObject(content, new ValueTypeAdapter<>(valueType));
+		} catch (Exception e) {
+			throw new JSONException(e.getMessage(), e);
+		} 
+	}
+	
+	/**
+     * Method that can be used to serialize any JSON output as
+     * Java value.
+     */
+	public static <T> T readValue(byte[] src, ValueType<T> valueType) throws JSONException {
+		try {
+			return JSON.parseObject(src, valueType.getType());
+		} catch (Exception e) {
+			throw new JSONException(e.getMessage(), e);
+		} 
+	}
+	
+	/**
+     * Method that can be used to serialize any JSON output as
+     * Java value.
+     */
+	public static <T> T readValue(InputStream src, ValueType<T> valueType) throws JSONException {
+		try {
+			return JSON.parseObject(IOUtils.toByteArray(src), valueType.getType());
+		} catch (Exception e) {
+			throw new JSONException(e.getMessage(), e);
+		} 
 	}
 }
--- xultimate-core/src/main/java/org/danielli/xultimate/core/json/fastjson/FastJSONTemplate.java
+++ xultimate-core/src/main/java/org/danielli/xultimate/core/json/fastjson/FastJSONTemplate.java
@@ -5,11 +5,8 @@ import java.io.OutputStream;
 
 import org.danielli.xultimate.core.json.JSONException;
 import org.danielli.xultimate.core.json.JSONTemplate;
+import org.danielli.xultimate.core.json.JsonUtils;
 import org.danielli.xultimate.core.json.ValueType;
-import org.danielli.xultimate.core.json.fastjson.ValueTypeAdapter;
-import org.danielli.xultimate.util.io.IOUtils;
-
-import com.alibaba.fastjson.JSON;
 
 /**
  * JSON模板类，使用FastJSON实现。
@@ -22,55 +19,31 @@ public class FastJSONTemplate implements JSONTemplate {
 
 	@Override
 	public <T> String writeValueAsString(T value) throws JSONException {
-		try {
-			return JSON.toJSONString(value);
-		} catch (Exception e) {
-			throw new JSONException(e.getMessage(), e);
-		} 
+		return JsonUtils.writeValueAsString(value);
 	}
-
+	
 	@Override
 	public <T> byte[] writeValueAsBytes(T value) throws JSONException {
-		try {
-			return JSON.toJSONBytes(value);
-		} catch (Exception e) {
-			throw new JSONException(e.getMessage(), e);
-		} 
+		return JsonUtils.writeValueAsBytes(value);
 	}
 
 	@Override
 	public <T> void writeValue(OutputStream out, T value) throws JSONException {
-		try {
-			IOUtils.write(JSON.toJSONBytes(value), out);
-		} catch (Exception e) {
-			throw new JSONException(e.getMessage(), e);
-		} 
+		JsonUtils.writeValue(out, value);
 	}
 
 	@Override
 	public <T> T readValue(String content, ValueType<T> valueType) throws JSONException {
-		try {
-			return JSON.parseObject(content, new ValueTypeAdapter<>(valueType));
-		} catch (Exception e) {
-			throw new JSONException(e.getMessage(), e);
-		} 
+		return JsonUtils.readValue(content, valueType);
 	}
-
+	
 	@Override
 	public <T> T readValue(byte[] src, ValueType<T> valueType) throws JSONException {
-		try {
-			return JSON.parseObject(src, valueType.getType());
-		} catch (Exception e) {
-			throw new JSONException(e.getMessage(), e);
-		} 
+		return JsonUtils.readValue(src, valueType);
 	}
 
 	@Override
 	public <T> T readValue(InputStream src, ValueType<T> valueType) throws JSONException {
-		try {
-			return JSON.parseObject(IOUtils.toByteArray(src), valueType.getType());
-		} catch (Exception e) {
-			throw new JSONException(e.getMessage(), e);
-		} 
+		return JsonUtils.readValue(src, valueType);
 	}
 }
