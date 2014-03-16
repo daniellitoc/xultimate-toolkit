d3e70673f8262274ff38d891b74bd3786076a22c
Daniel Li <daniellitoc@gmail.com>
2014-03-16 14:03:13 +0800

    去除SQL文件中的数字的精度。

--- xultimate-orm/xultimate-mybatis/src/test/resources/mybatis_init.sql
+++ xultimate-orm/xultimate-mybatis/src/test/resources/mybatis_init.sql
@@ -1,31 +1,31 @@
 CREATE TABLE IF NOT EXISTS `ULTIMATE_AREA` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `po_version` bigint(20) DEFAULT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `po_version` bigint DEFAULT NULL,
   `create_time` timestamp NULL DEFAULT NULL,
   `update_time` timestamp NULL DEFAULT NULL,
   `boost` float DEFAULT NULL,
   `display_name` longtext NOT NULL,
   `name` varchar(255) NOT NULL,
-  `parent_id` bigint(20) DEFAULT NULL,
+  `parent_id` bigint DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `ix_parentId` (`parent_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `TEST_PO` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `parent_id` bigint(20) NOT NULL,
-  `other_id` int(11) NOT NULL,
-  `login_count` int(11) DEFAULT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `parent_id` bigint NOT NULL,
+  `other_id` int NOT NULL,
+  `login_count` int DEFAULT NULL,
   `boost` float DEFAULT NULL,
-  `money` bigint(20) DEFAULT NULL,
+  `money` bigint DEFAULT NULL,
   `message` varchar(255) NOT NULL,
-  `sex` tinyint(6) DEFAULT NULL,
+  `sex` tinyint DEFAULT NULL,
   `introduction` text NOT NULL,
   `create_time` timestamp NULL DEFAULT NULL,
   `update_time` timestamp NULL DEFAULT NULL,
   `is_lock` char(0) DEFAULT NULL,
   `has_login` bool DEFAULT NULL,
-  `state_set` tinyint(6) DEFAULT NULL,
-  `login_ip` int(20) unsigned DEFAULT NULL,
+  `state_set` tinyint DEFAULT NULL,
+  `login_ip` int unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
