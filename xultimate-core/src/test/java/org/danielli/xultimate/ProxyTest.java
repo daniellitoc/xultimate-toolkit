c2687b495af012c835f930e21eb478adc16c4212
Daniel Li <daniellitoc@gmail.com>
2014-03-22 23:11:11 +0800

    添加jdk和cglib的测试，主要用来模拟动态数据源那。

---
+++ xultimate-core/src/test/java/org/danielli/xultimate/ProxyTest.java
@@ -0,0 +1,94 @@
+package org.danielli.xultimate;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import net.sf.cglib.proxy.Enhancer;
+
+import org.danielli.xultimate.util.performance.PerformanceMonitor;
+import org.danielli.xultimate.util.time.stopwatch.support.AdvancedStopWatchSummary;
+import org.junit.Test;
+
+public class ProxyTest {
+	
+	private Map<Integer, ProxyObject> proxyObjects = new ConcurrentHashMap<Integer, ProxyObject>();
+	private ThreadLocal<Integer> keyThreadLocal = new ThreadLocal<>();
+
+	class JDKInvocationHandler implements InvocationHandler {
+
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+			ProxyObject object = proxyObjects.get(keyThreadLocal.get());
+			return method.invoke(object, args);
+		}
+	}
+	
+	class CglibInvocationHandler implements net.sf.cglib.proxy.InvocationHandler {
+
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+			ProxyObject object = proxyObjects.get(keyThreadLocal.get());
+			return method.invoke(object, args);
+		}
+	}
+	
+	@Test
+	public void test() {
+		for (int i = 0; i < 100; i++) {
+			proxyObjects.put(i, new DefaultProxyObject(i));
+		}
+		
+		ProxyObject jdkObject = (ProxyObject) Proxy.newProxyInstance(ProxyObject.class.getClassLoader(), new Class<?>[] { ProxyObject.class }, new JDKInvocationHandler());
+	
+		Enhancer enhancer = new Enhancer();
+		enhancer.setInterfaces(new Class<?>[] { ProxyObject.class });
+		enhancer.setCallback(new CglibInvocationHandler());
+		ProxyObject cglibObject = (ProxyObject) enhancer.create();
+		
+		PerformanceMonitor.start("ProxyTest");
+		
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 1000000; j++) {
+				keyThreadLocal.set(j % 100);
+				jdkObject.getProxy();
+			}
+			PerformanceMonitor.mark("jdkObject" + i);
+		}
+		
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 1000000; j++) {
+				keyThreadLocal.set(j % 100);
+				cglibObject.getProxy();
+			}
+			PerformanceMonitor.mark("cglibObject" + i);
+		}
+		PerformanceMonitor.stop();
+		PerformanceMonitor.summarize(new AdvancedStopWatchSummary(true));
+		PerformanceMonitor.remove();
+		
+	}
+}
+
+
+
+
+interface ProxyObject {
+	String getProxy();
+}
+
+class DefaultProxyObject implements ProxyObject {
+	
+	private Integer value;
+	
+	public DefaultProxyObject(Integer value) {
+		this.value = value;
+	}
+	
+	@Override
+	public String getProxy() {
+		return "Proxy" + value;
+	}
+}
