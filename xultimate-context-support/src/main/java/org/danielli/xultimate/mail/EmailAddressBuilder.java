02da18f3edecd4a9a8234b078e1574ace6cee5c1
Daniel Li <daniellitoc@gmail.com>
2014-04-11 22:49:16 +0800

    添加EmailAddressBuilder代替配置文件中使用InternetAddress.toString()的方式。

---
+++ xultimate-context-support/src/main/java/org/danielli/xultimate/mail/EmailAddressBuilder.java
@@ -0,0 +1,42 @@
+package org.danielli.xultimate.mail;
+
+import javax.mail.internet.InternetAddress;
+
+import org.apache.commons.lang3.builder.Builder;
+import org.danielli.xultimate.util.CharsetUtils;
+import org.springframework.mail.MailParseException;
+
+public class EmailAddressBuilder implements Builder<String> {
+
+    protected String address;
+
+    protected String personal;
+    
+    protected String charset = CharsetUtils.CharEncoding.UTF_8;
+    
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setPersonal(String personal) {
+		this.personal = personal;
+	}
+
+	public void setCharset(String charset) {
+		if (CharsetUtils.isSupported(charset)) {
+			this.charset = charset;
+		}
+	}
+	
+	@Override
+	public String build() {
+		InternetAddress internetAddress = new InternetAddress();
+		internetAddress.setAddress(address);
+		try {
+			internetAddress.setPersonal(personal, charset);
+		} catch (Exception e) {
+			throw new MailParseException(e.getMessage(), e);
+		}
+		return internetAddress.toString();
+	}
+}
